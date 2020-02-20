3cfea66d01d7fd7540ed5d7a164de62119b047cf
rbarakhvostov <rbarakhvostov@gmail.com>
2020-02-20 18:24:57 +0300

    add scss support

---
+++ src/components/App/App.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import './app.scss';
+
+export default class App extends Component {
+  state = {
+    messages: null,
+  }
+
+  render() {
+    return (
+      <div className='chat'>
+        
+
+      </div>
+    )
+  }
+}
