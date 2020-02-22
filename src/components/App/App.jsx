f892c94ee2d361ce0d56ba4f65b26238d0431aa7
rbarakhvostov <rbarakhvostov@gmail.com>
2020-02-22 22:07:41 +0300

    reorder messages

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,19 +8,28 @@ import './app.scss';
 
 export default class App extends Component {
   _protocol = 'wss://wssproxy.herokuapp.com/';
-
+  socket = new WebSocket(this._protocol);
   state = {
     messages: [],
     status: 'online',
   }
 
   componentDidMount = () => {
-    const socket = new WebSocket(this._protocol);
-    socket.onmessage = (event) => {
-      this.setState({ 
-        messages: JSON.parse(event.data),
+    // const socket = new WebSocket(this._protocol);
+    this.socket.onmessage = (event) => {
+      console.log(JSON.parse(event.data))
+      this.setState(({ messages }) => {
+        return {
+          messages: [...messages, ...JSON.parse(event.data).reverse()],
+        }
       }); 
     }
+
+    this.socket.onclose = event => {
+      console.log('1', event.reason)
+      console.log('2', event.code)
+      console.log('3', event.wasClean)
+    };
   }
 
   handleMessageAdd = (messageText) => {
