b6f401dcc0c6ca68840e0ac19cf4716ef1ccf94b
rbarakhvostov <rbarakhvostov@gmail.com>
2020-02-26 13:59:37 +0300

    change project structure

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,50 +1,14 @@
 import React, { Component } from 'react';
 import LoginForm from '../LoginForm';
-import Header from '../Header';
-import Loader from '../Loader';
-import MessageField from '../MessageField';
-import MessageForm from '../MessageForm';
+import Chat from '../Chat';
 // import WSservice from '../../service/ws-service';
 
 import './app.scss';
 
 export default class App extends Component {
-  _protocol = 'wss://wssproxy.herokuapp.com/';
-  socket = null;
   state = {
     userName: localStorage.getItem("WSchatUserName"),
-    loading: true,
-    messages: [],
     status: 'online',
-  }
-
-  connect = () => {
-    this.socket = new WebSocket(this._protocol);
-
-    this.socket.onmessage = (event) => {
-      if (
-        JSON.stringify(this.state.messages) 
-          === 
-        JSON.stringify(JSON.parse(event.data).reverse())
-        ) {
-        return;
-      }
-      this.setState(({ messages }) => {
-        return {
-          loading: false,
-          messages: [...messages, ...JSON.parse(event.data).reverse()],
-        }
-      }); 
-    }
-
-    this.socket.onclose = () => {
-      console.log(this.socket.readyState);
-      this.connect();
-    };
-  }
-
-  componentDidMount = () => {
-    this.connect();
   }
 
   handleUserLogIn = (userName) => {
@@ -61,32 +25,19 @@ export default class App extends Component {
     });
   }
 
-  handleMessageSend = (messageText) => {
-    console.log(messageText);
-    const message = {
-      from: this.state.userName,
-      message: messageText,
-    }
-    this.socket.send(JSON.stringify(message));
-  }
-
   render() {
-    const { userName, loading, messages, status } = this.state;
-
-    if (!userName) {
-      return <LoginForm onUserLogIn={ this.handleUserLogIn }/>
-    }
+    const { userName, status } = this.state;
 
     return (
       <div className='app'>
-        <Header
-          userName={ userName }
-          status={ status }
-          onUserLogOut={ this.handleUserLogOut } />
-        <div className='content'>
-          { loading ? <Loader /> : <MessageField messages={ messages } /> }
-        </div>
-        <MessageForm onMessageSend={ this.handleMessageSend } />
+        { 
+          !userName 
+            ? <LoginForm onUserLogIn={ this.handleUserLogIn } />
+            : <Chat 
+                status={ status }
+                userName={ userName }
+                onUserLogOut={ this.handleUserLogOut } />
+        }
       </div>
     );
   }
---
+++ src/components/Chat/Chat.jsx
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import Header from '../Header';
+import Loader from '../Loader';
+import MessageField from '../MessageField';
+import MessageForm from '../MessageForm';
+
+import './chat.scss';
+
+export default class Chat extends Component {
+  _protocol = 'wss://wssproxy.herokuapp.com/';
+  socket = null;
+  state = {
+    loading: true,
+    messages: [],
+  }
+
+  connect = () => {
+    this.socket = new WebSocket(this._protocol);
+
+    this.socket.onmessage = (event) => {
+      console.log('new MESSAGE')
+      if (
+        JSON.stringify(this.state.messages) 
+          === 
+        JSON.stringify(JSON.parse(event.data).reverse())
+        ) {
+        return;
+      }
+      this.setState(({ messages }) => {
+        return {
+          loading: false,
+          messages: [...messages, ...JSON.parse(event.data).reverse()],
+        }
+      }); 
+    }
+
+    this.socket.onclose = (event) => {
+      console.log(event.reason);
+      console.log(this.socket.readyState);
+      if (event.reason === 'the work is done') return;
+      this.connect();
+    };
+  }
+
+  componentDidMount = () => {
+    this.connect();
+  }
+
+  componentWillUnmount = () => {
+    console.log('UNMOUNT')
+    this.socket.close(1000, 'the work is done');
+  }
+
+  handleMessageSend = (messageText) => {
+    console.log(messageText);
+    const message = {
+      from: this.props.userName,
+      message: messageText,
+    }
+    this.socket.send(JSON.stringify(message));
+  }
+
+  render() {
+    const { loading, messages } = this.state;
+    const { userName, status, onUserLogOut } = this.props;
+
+    return (
+      <>
+        <Header
+          userName={ userName }
+          status={ status }
+          onUserLogOut={ onUserLogOut } />
+        <div className='content'>
+          { loading ? <Loader /> : <MessageField messages={ messages } /> }
+        </div>
+        <MessageForm onMessageSend={ this.handleMessageSend } />
+      </>
+    );
+  }
+}
